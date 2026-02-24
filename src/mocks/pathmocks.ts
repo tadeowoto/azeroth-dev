466187fb84d32c31b5be1b873a91b0929bc865c5
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-02-24 12:13:04 -0300

    mock for data of paths

---
+++ src/mocks/pathmocks.ts
@@ -0,0 +1,51 @@
+
+export const pathMock = [
+    {
+        id: 1,
+        description: "Learn JavaScript from scratch and build a simple web application",
+        slug: "learn-js",
+        title: "Learn JavaScript",
+    },
+    {
+        id: 2,
+        description: "Learn React from scratch and build a simple web application",
+        slug: "learn-react",
+        title: "Learn React",
+    },
+    {
+        id: 3,
+        description: "Learn Next.js from scratch and build a simple web application",
+        slug: "learn-nextjs",
+        title: "Learn Next.js",
+    },
+    {
+        id: 4,
+        description: "Learn Node.js from scratch and build a simple web application",
+        slug: "learn-nodejs",
+        title: "Learn Node.js",
+    },
+    {
+        id: 5,
+        description: "Learn Python from scratch and build a simple web application",
+        slug: "learn-python",
+        title: "Learn Python",
+    },
+    {
+        id: 6,
+        description: "Learn Java from scratch and build a simple web application",
+        slug: "learn-java",
+        title: "Learn Java",
+    },
+    {
+        id: 7,
+        description: "Learn C++ from scratch and build a simple web application",
+        slug: "learn-cpp",
+        title: "Learn C++",
+    },
+    {
+        id: 8,
+        description: "Learn C# from scratch and build a simple web application",
+        slug: "learn-csharp",
+        title: "Learn C#",
+    },
+]
