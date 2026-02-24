6671b2a6eed98f130086b0e699c29a4942f81378
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-02-24 10:47:36 -0300

    warcraftcn components, button and badge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import react from "@astrojs/react";
 
 // https://astro.build/config
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   },
 
-  integrations: [react()]
+  integrations: [react()],
 });
