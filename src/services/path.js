ab42a454814137f64f46665978ba23bd7367c2ed
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-02 12:01:34 -0300

    statics pages generated

--- src/services/path.js
+++ src/services/path.js
@@ -1,4 +1,20 @@
-const res = await fetch("http://localhost:8080/api/path");
-const paths = await res.json();
+export const getPaths = async () => {
+  const res = await fetch("http://localhost:8080/api/path");
+  const paths = await res.json();
 
-export default paths;
+  return paths;
+};
+
+export const getPathBySlug = async (slug) => {
+  const res = await fetch(`http://localhost:8080/api/path/${slug}`);
+  const path = await res.json();
+
+  return path;
+};
+
+export const getAllSlugs = async () => {
+  const res = await fetch("http://localhost:8080/api/path/all");
+  const slugs = await res.json();
+
+  return slugs;
+};
